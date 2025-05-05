0e722079461d85bd2ea11e77407519959dfec4ba
Sanju Peramuna <sanju.peramuna@gmail.com>
2025-05-05 14:20:03 +0530

    Add GTM integration with NEXT_PUBLIC_GTM_ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+            })(window,document,'script','dataLayer','GTM-MRD82LQD');
           `}
         </Script>
       </head>
@@ -36,7 +36,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MRD82LQD"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
